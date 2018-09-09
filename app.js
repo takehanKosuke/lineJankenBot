726034b05b239a72afbfa40e4ad06640e74808aa
takehanKosuke <kkoussuke530@icloud.com>
2018-09-09 14:52:21 +0900

    app2

--- app.js
+++ app.js
@@ -1,3 +1,46 @@
+// const path = require("path");
+// // フレームワークの繋げるやつ
+// const express = require("express");
+// // LineのAPI
+// const line = require("@line/bot-sdk");
+
+// // Lineのアクセストークン
+// const lineConfig = {
+//   channelAccessToken: process.env.LINE_CHANNEL_ACCESS_TOKEN,
+//   channelSecret: process.env.LINE_CHANNEL_SECRET
+// };
+// const lineClient = new line.Client(lineConfig);
+
+
+// function createReplyMessage(input) {
+//   // 1. 固定メッセージを返す
+//   return {
+//     type: "text",
+//     text: "暗殺教室面白いよね！！"
+//   };
+// }
+
+// const server = express();
+// // サーバーから画像を返す
+// server.use("/images", express.static(path.join(__dirname, "images")));
+
+// // ここでlineにpostする
+// server.post("/webhook", line.middleware(lineConfig), (req, res) => {
+//   // LINEのサーバーに200を返す
+//   res.sendStatus(200);
+
+//   for (const event of req.body.events) {
+//     if (event.type === "message" && event.message.type === "text") {
+//       const message = createReplyMessage(event.message.text);
+//       lineClient.replyMessage(event.replyToken, message);
+//     }
+//   }
+// });
+
+// server.listen(process.env.PORT || 8080);
+
+
+
 const path = require("path");
 const express = require("express");
 const line = require("@line/bot-sdk");
@@ -9,10 +52,14 @@ const lineConfig = {
 const lineClient = new line.Client(lineConfig);
 
 function createReplyMessage(input) {
-  // 1. 固定メッセージを返す
+  // 2. オウム返しする
   return {
     type: "text",
-    text: "暗殺教室面白いよね！！"
+    // `（バッククォート）で囲った中で${変数名}や${式}を書くと結果が展開される
+    // テンプレートリテラル（Template literal）という文法です
+    text: `${input}、と言いましたね？`
+    // 以下と同じです
+    // text: input + '、と言いましたね？'
   };
 }
 
