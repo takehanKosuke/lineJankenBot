64fe0da7d9f822ab7db80cf7d07b02c5716ad236
takehanKosuke <kkoussuke530@icloud.com>
2018-09-09 16:52:31 +0900

    tweak

--- app.js
+++ app.js
@@ -9,20 +9,38 @@ const lineConfig = {
 const lineClient = new line.Client(lineConfig);
 
 function createReplyMessage(input) {
-  let title
-  let text
-  let actions
-  $columns = array(
-    array(title   => 'タイトル最大４０文字',
-          text    => 'タイトルか画像がある場合は最大60文字、どちらもない場合は最大120文字',
-          actions => array(array(type => message, label => ラベルです, text => メッセージ)) 
-    ),
-    array(title   => 'タイトル最大４０文字',
-          text    => 'タイトルか画像がある場合は最大60文字、どちらもない場合は最大120文字',
-          actions => array(array(type => message, label => ラベルです, text => メッセージ)) 
-    )            
-);
+  // 3. 条件分岐（じゃんけん）
+  const hands = ["グー", "チョキ", "パー"];
+  // 返信メッセージを入れる変数
+  let text;
+
+  youHand = hands.indexOf(input)
+  // 配列.indexOf(引数) =>
+  //   引数が配列の何番目（0始まり）にあるかを返す
+  //   引数が配列にない場合、-1を返す
+  if (youHand === -1) {
+    // ユーザーが入力した内容が「グー、チョキ、パー」以外だった場合
+    text = "グー・チョキ・パーのどれかを入力してね";
+  } else {
+    // 手からランダムに一つ選択
+    i = Math.floor(hands.length * Math.random())
+    hand = hands[i];
+    if (i - youHand === 1) {
+      text = `${hand}を出しました。あなたの勝ちです`
+    }else if(i - youHand === 0){
+      text = `${hand}を出しました。あいこです`
+    }else{
+      text = `${hand}を出しました。あなたの負けです`
+    }
   }
+
+  return {
+    type: "text",
+    // 「text: text」のようにキー名と変数名が同じ場合、以下のように省略可能
+    // Object Shorthandという文法です
+    text
+  };
+}
 
 const server = express();
 
